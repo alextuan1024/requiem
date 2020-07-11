516ef5b345184c50f1615f4f6d54d2ede17c6813
alextuan1024 <alextuan1024@gmail.com>
2020-07-11 20:56:44 +0800

    delete test api

--- web/v1/router.go
+++ web/v1/router.go
@@ -1,9 +1,7 @@
 package v1
 
 import (
-	"github.com/alextuan1024/requiem/web"
 	"github.com/gin-gonic/gin"
-	"net/http"
 	"sync"
 )
 
@@ -20,15 +18,5 @@ func GetApi() *Api {
 	return api
 }
 
-func (a *Api) handlerFoo(c *gin.Context) {
-	logger().Info("foo")
-	c.String(http.StatusOK, "Bar")
-}
-
 func RegisterHandlers(e *gin.Engine, a *Api) {
-	v1 := e.Group("/v1")
-	v1.POST("/login", web.LoginHandler)
-
-	v1a := v1.Use(web.AuthMiddleware)
-	v1a.GET("/foo", a.handlerFoo)
 }
